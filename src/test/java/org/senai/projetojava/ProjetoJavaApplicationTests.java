14fbd8dd7e2b41291197a463f36f183f98b704ce
vandersonbfs <vanderson_b@hotmail.com>
2022-11-15 21:02:29 -0300

    Inicial

---
+++ src/test/java/org/senai/projetojava/ProjetoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.senai.projetojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
