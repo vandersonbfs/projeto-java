d9ad53dfe1e2dc6166428cb6e1f02960488b1428
vandersonbfs <vanderson_b@hotmail.com>
2022-11-16 12:08:44 -0300

    Mevendo arquivo

--- src/main/java/org/senai/projetojava/UsuarioRepository.java
+++ src/main/java/org/senai/projetojava/UsuarioRepository.java
@@ -1,6 +1,5 @@
 package org.senai.projetojava;
 
-import org.senai.Usuario;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, Integer> {
